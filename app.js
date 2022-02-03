abf8d2e594875a7751011b1bccad6c401e63d26a
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-03 23:16:04 +0700

    learn creating new path with nodeJS

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const http = require("http");
 
-const server = http.createServer();
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    const date = new Date().toISOString();
+    response.statusCode = 200;
+    response.end(`<h1>${date}</h1>`);
+  } else if (request.url === "/") {
+    // localhost:3000/currenttime
+    // localhost:3000
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  }
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
