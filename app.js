871a33c4fab3d634c2d5e08977ea5b58002aa046
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-04 11:42:43 +0700

    replacing route handler from nodeJS to expressJS

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const http = require("http");
+const express = require("express");
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    const date = new Date().toISOString();
-    response.statusCode = 200;
-    response.end(`<h1>${date}</h1>`);
-  } else if (request.url === "/") {
-    // localhost:3000/currenttime
-    // localhost:3000
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const app = express();
 
-const server = http.createServer(handleRequest);
+app.get("/currenttime", function (req, res) {
+  const date = new Date().toISOString();
+  res.send(`<h1>${date}</h1>`);
+});
 
-server.listen(3000);
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000);
+
+// function handleRequest(request, response) {
+//   if (request.url === "/currenttime") {
+//     const date = new Date().toISOString();
+//     response.statusCode = 200;
+//     response.end(`<h1>${date}</h1>`);
+//   } else if (request.url === "/") {
+//     // localhost:3000/currenttime
+//     // localhost:3000
+//     response.statusCode = 200;
+//     response.end("<h1>Hello World!</h1>");
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(3000);
